b6d62d08d4a77ec0077805aa8c513ca79df4f762
garciabarbaraa65998 <garcia_barbaraa65998@gmx.com>
2025-03-08 22:35:15 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,8 @@
+
+<script>
+const num1 = Math.floor(Math.random() * 10);
+const num2 = Math.floor(Math.random() * 10);
+const sum = num1 + num2;
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+</script>
